766377b012a9c5b50679b6d34df605b051db737c
anil94sahu <anil94sahu@gmail.com>
2024-02-21 23:03:46 +0530

    http routing

--- http.js
+++ http.js
@@ -22,13 +22,34 @@ const obj = {
     res.end(JSON.stringify(obj))
 }) */
 
-const server = http.createServer((req,res) => {
+/* const server = http.createServer((req,res) => {
     const name = "Anil"
     res.writeHead(200, {"Content-Type": "text/html"});
     let html = fs.readFileSync('./index.html', "utf-8");
     html = html.replace("{{name}}", name)
     // fs.createReadStream(__dirname + '/index.html').pipe(res);
     res.end(html)
+}) */
+
+
+const server = http.createServer((req, res) => {
+    if(req.url === '/'){
+        res.writeHead(200, {"Content-Type": "text/plain"});
+        res.end("Home page ")
+    } else if(req.url === '/about'){
+        res.writeHead(200, {"Content-Type": "text/plain"});
+        res.end("About page")   
+    }
+    else if(req.url === '/api'){
+        res.writeHead(200, {"Content-Type": "application/json"});
+        res.end(JSON.stringify({
+            firstName: "Bruce",
+            lastName: "Laynce"
+        }))   
+    } else {
+        res.writeHead(404, {"Content-Type": "text/plain"});
+        res.end("404 page")
+    }
 })
 
 server.listen(3000, () => console.log('server listen in 3000'))

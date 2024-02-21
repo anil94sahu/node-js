5fd101c449a10d6af28c23dfd50719aaaaa3023b
anil94sahu <anil94sahu@gmail.com>
2024-02-21 22:54:11 +0530

    dynamic html added

--- http.js
+++ http.js
@@ -3,20 +3,32 @@
 // We can't send object drectly, so convert our data into json stringify
 
 const http = require('node:http');
+const fs = require('node:fs');
 
 /* const server = http.createServer((req, res) => {
     res.writeHead(200, {"Content-Type": "text/plain"});
     res.end('end on content')
 }) */
 
+
+
 const obj = {
     firstName: 'Anil',
     lastName: 'sahu'
 }
 
-const server = http.createServer((req, res) => {
+/* const server = http.createServer((req, res) => {
     res.writeHead(200, {"Content-Type": "application/json"});
     res.end(JSON.stringify(obj))
+}) */
+
+const server = http.createServer((req,res) => {
+    const name = "Anil"
+    res.writeHead(200, {"Content-Type": "text/html"});
+    let html = fs.readFileSync('./index.html', "utf-8");
+    html = html.replace("{{name}}", name)
+    // fs.createReadStream(__dirname + '/index.html').pipe(res);
+    res.end(html)
 })
 
 server.listen(3000, () => console.log('server listen in 3000'))
